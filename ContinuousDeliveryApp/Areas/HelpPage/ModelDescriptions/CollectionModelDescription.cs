8ae8587810593c9109699cfb156eabf9caafeafa
Richard Lewis <ricklewis_axa24@outlook.com>
2016-04-10 14:37:15 -0400

    1

---
+++ ContinuousDeliveryApp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ContinuousDeliveryApp.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ContinuousDeliveryApp/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ContinuousDeliveryApp.Startup))]
+
+namespace ContinuousDeliveryApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
